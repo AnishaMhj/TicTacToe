9ffccddcebaae13734e149fdea8baef2943ed1c1
anishmhrzn47 <anishamhrzn47@gmail.com>
2022-12-13 11:23:54 +1100

    Create a copy of the squares array to modify using .slice()

--- src/components/board.js
+++ src/components/board.js
@@ -13,6 +13,15 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
         };
     }
+
+    //event handler to handle click of a Square
+    //.slice() is called to create a copy of the squares array to modify instead of modifying the existing array
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
     //Passing two props 'value' and 'onClick' from Board to Square
     renderSquare(i) {
         return (
