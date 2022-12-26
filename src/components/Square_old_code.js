6f27547b79520db902487c464f00da4bcba42e52
anishmhrzn47 <anishamhrzn47@gmail.com>
2022-12-26 21:10:53 +1100

    Change Square into a functional component

---
+++ src/components/Square_old_code.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+// Square renders a single button
+
+
+class Square extends React.Component {
+
+    //1. constructor to initialize the state: CLASS BASE COMPONENT / STATELESS COMPONENTS
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: null
+    //     };
+    // }
+    //   FUNCTIONAL COMPONENT
+    // const [mark, setMark] = useState(0);
+
+    render() {
+        return (
+            /*  2. onClick handler calls this.setState which will tell React to re-render that Square whenever its button is clicled.
+            Upon update the Square's this.state.value will be 'X' */
+            // <button className="square"
+            //     onClick={() => this.setState({ value: 'X' })
+            //     }>
+            //     {this.state.value}
+            // </button>
+
+            //3. will be changed into this, while the constructor above is deleted, or in our case commented out
+            <button className="square"
+                onClick={() => this.props.onClick()
+                }>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+export default Square;
--- src/components/square.js
+++ src/components/square.js
@@ -2,36 +2,15 @@ import React from "react";
 
 // Square renders a single button
 
+//4. changing the Square into a functional component
+//fn takes props as input and returns what should be rendered
+function Square(props) {
 
-class Square extends React.Component {
+    return (
 
-    //constructor to initialize the state: CLASS BASE COMPONENT / STATELESS COMPONENTS
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null
-    //     };
-    // }
-    //   FUNCTIONAL COMPONENT
-    // const [mark, setMark] = useState(0);
-
-    render() {
-        return (
-            /*  onClick handler calls this.setState which will tell React to re-render that Square whenever its button is clicled.
-            Upon update the Square's this.state.value will be 'X' */
-            // <button className="square"
-            //     onClick={() => this.setState({ value: 'X' })
-            //     }>
-            //     {this.state.value}
-            // </button>
-
-            //will be changed into this, while the constructor above is deleted, or in our case commented out
-            <button className="square"
-                onClick={() => this.props.onClick()
-                }>
-                {this.props.value}
-            </button>
-        );
-    }
+        <button className="square"
+            onClick={props.onClick}>{props.value}
+        </button>
+    );
 }
 export default Square;
