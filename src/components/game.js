2245672be72aaf858a9b1cf6f9e1dcb6206f442d
anishmhrzn47 <anishamhrzn47@gmail.com>
2022-12-31 11:01:36 +1100

    Implement go back in time, by clicking on any step in the game's history, the board updates to show what the board looked like after that step occured

--- src/components/game.js
+++ src/components/game.js
@@ -11,6 +11,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,      // indicates current view step
             xIsNext: true,      //sets the first move to 'X' by default
         };
     }
@@ -20,7 +21,7 @@ class Game extends React.Component {
     //event handler to handle click of a Square
     //.slice() is called to create a copy of the squares array to modify instead of modifying the existing array
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (CalculateWinner(squares) || squares[i]) {
@@ -31,17 +32,26 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,        //function to flip the value of xIsNext
         });
     }
 
+    //jumpTo method to update the stepNumber and
+    //set xIsNext to true if the number that we're changing stepNumber to is even 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) == 0,
+        });
+    }
 
     render() {
 
         //Since the Game component rendered the game's status, remove the corresponding code from Board's render method 
         //update render to use the most recent history entry to determiine and display game's status
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = CalculateWinner(current.squares);
 
         //Mapping history of moves using map method
